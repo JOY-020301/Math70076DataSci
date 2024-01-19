06c050008e5f27d8b132233bad5ba84f2d084183
Joy Hu <97757792+JOY-020301@users.noreply.github.com>
2024-01-19 14:23:29 +0000

    fucntion_update

--- rolling_arithmetic_mean.R
+++ rolling_arithmetic_mean.R
@@ -1,16 +1,32 @@
-roll_ari_mean <- function(data, windowSize) {
-  # Check if the data is a numeric vector
-  if (!is.numeric(data)) {
-    stop("Data must be a numeric vector.")
+rolling_mean <- function(x, window_width, ...){
+  # -----Input Checks ----------------------------------------------------------
+  # Check that x is a vector with numerical interpretation
+  stopifnot(is.logical(x) | is.integer(x) | is.double(x) | is.complex(x))
+  stopifnot(length(x) > 0)
+  
+  # Check window_width is an odd, positive integer
+  stopifnot(length(window_width) == 1)
+  stopifnot(window_width %% 1 == 0)
+  stopifnot((window_width / 2) %% 1 != 0)
+  stopifnot(window_width > 0)
+  
+  # ----- Function Body --------------------------------------------------------
+  
+  # number of values left and right to include in each mean
+  half_width <- floor(window_width / 2)
+  x_padded <- pad_with_NAs(x, n_left = half_width, n_right = half_width)
+  evaluation_locations <- seq_along(x) + half_width
+  
+  output <- rep(NA, length(x))
+  
+  for (index in evaluation_locations) {
+    # Extract relevant values from x_padded
+    indices_in_window <- seq(index - half_width, index + half_width, by = 1)
+    values_in_window <- x_padded[indices_in_window]
+    
+    # Calculate and store mean
+    output[index - half_width] <- mean(values_in_window, ...)
   }
   
-  # Check if window size is valid
-  if (!is.numeric(windowSize) || windowSize <= 0 || windowSize > length(data)) {
-    stop("Invalid window size.")
-  }
-  
-  # Calculate the rolling mean
-  rollingMean <- filter(data, rep(1/windowSize, windowSize), sides=1)
-  
-  return(rollingMean)
+  return(output)
 }
